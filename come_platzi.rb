e91ac900ca46cf0f3a6a993c61285733bc15f102
Miguel Alejandro <mvinan@icloud.com>
2015-06-03 23:30:20 -0500

    update commit probando hooks

--- come_platzi.rb
+++ come_platzi.rb
@@ -1,11 +1,9 @@
 cmd = ARGV.shift
 if cmd
 	alimento = cmd
-
 	File.open('alimentos.txt', 'a') do |f|
 		creado = Time.now
 		f.puts "#{alimento}, #{creado}"
 	end
-
 	puts "#{alimento} agregado"
 end
